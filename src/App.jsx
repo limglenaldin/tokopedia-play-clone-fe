653ec183ee2c2aa9181688e6664be4563c1307da
Glenaldin Halim <glenaldinhalim16@gmail.com>
2023-08-08 23:33:37 +0700

    initial implement chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Built-in Libraries
+// Third-party Libraries
+import {ChakraProvider, Flex, Heading, Text } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ChakraProvider>
+      <Flex bg="green.500" minW="full" minH='100vh' direction="column">
+        <Heading as='h1' size='lg'>Hello from ChakraUI</Heading>
+        <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio natus, id dicta consequuntur iure ratione magni? Deserunt, nam quae.</Text>
+      </Flex>
+    </ChakraProvider>
   )
 }
 
