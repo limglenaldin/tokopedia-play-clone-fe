cd88305fccd5fb9cc098c0fe82a3c3c283dc19ea
Glenaldin Halim <glenaldinhalim16@gmail.com>
2023-08-12 00:46:47 +0700

    modify detail page

--- src/pages/Videos/Detail.jsx
+++ src/pages/Videos/Detail.jsx
@@ -1,14 +1,96 @@
 // Built-in Libraries
 // Third-party Libraries
-import { Flex, Heading, Text } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { AspectRatio, Box, Flex, FormControl, Icon, IconButton, Input, Textarea } from "@chakra-ui/react";
+import { FiSend } from "react-icons/fi";
+
+// Components
+import ProductItem from "../../components/Card/Product";
+import CommentItem from "../../components/Card/Comment";
+
+const comments = [
+  {
+    "username" : "limglenaldin",
+    "comment" : "Lorem ipsum dolor amet Lorem ipsum dolor ametLorem ipsum dolor ametLorem ipsum dolor ametLorem ipsum dolor ametLorem ipsum dolor ametLorem ipsum dolor ametLorem ipsum dolor amet"
+  },
+  {
+    "username" : "limglenaldin",
+    "comment" : "Lorem ipsum dolor amet"
+  },
+  {
+    "username" : "limglenaldin",
+    "comment" : "Lorem ipsum dolor amet"
+  },
+  {
+    "username" : "limglenaldin",
+    "comment" : "Lorem ipsum dolor amet"
+  },
+  {
+    "username" : "limglenaldin",
+    "comment" : "Lorem ipsum dolor amet"
+  },
+  {
+    "username" : "limglenaldin",
+    "comment" : "Lorem ipsum dolor amet"
+  },
+]
 
 const Detail = () => {
   return (
-    <Flex bg="red.500" direction="column" minH='100vh'>
-      <Heading as='h1' size='lg'>Hello from Detail</Heading>
-      <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio natus, id dicta consequuntur iure ratione magni? Deserunt, nam quae.</Text>
-      <Link to="/">Go to Home</Link>
+    <Flex minH='100vh' direction="column">
+      <Flex direction="row" gap="4" mb={6}>
+        <Box w={9/12}>
+          <AspectRatio ratio={16/9}>
+            <iframe title="naruto" src="https://www.youtube.com/embed/QhBnZ6NPOY0" allowFullScreen />
+          </AspectRatio>
+        </Box>
+        <Flex w={3/12} h bgColor="gray.600" p="2" direction="column" borderRadius="xl" justify="space-between">
+          <Flex direction="column" gap="1" overflowY="auto" maxH="sm">
+            {
+              comments.map((comment, i) => (
+                <CommentItem
+                  key={i}
+                  data={comment}
+                />
+              ))
+            }
+          </Flex>
+          <Box borderTop="1px" pt="4">
+            <FormControl display="flex" gap="2" flexDirection="column" alignItems="end">
+              <Input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Username"
+              />
+              <Textarea
+                placeholder="Comment"
+              />
+              <Box>
+                <IconButton
+                  aria-label='Send'
+                  icon={<Icon as={FiSend} />}
+                  variant='outline'
+                  colorScheme='teal'
+                  />
+              </Box>
+            </FormControl>
+          </Box>
+        </Flex>
+      </Flex>
+      <Flex overflowX="auto" gap={2} pb="4">
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+      </Flex>
     </Flex>
   )
 }
