e003136494d58c655644fac6f83098df85e4c85a
Glenaldin Halim <glenaldinhalim16@gmail.com>
2023-08-12 19:39:01 +0700

    add axios instance

---
+++ src/api/apiInstance.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const BASE_API_URL = import.meta.env.VITE_BASE_API_URL
+
+const apiV1 = axios.create({
+    baseURL: `${BASE_API_URL}/api/v1`,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+export default apiV1;
