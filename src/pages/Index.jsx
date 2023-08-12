febb9d054637b1a2290e21c24e4a691ae51b7815
Glenaldin Halim <glenaldinhalim16@gmail.com>
2023-08-12 19:42:48 +0700

    connect index page to api

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,31 +1,57 @@
 // Built-in Libraries
+import { useEffect, useState } from "react";
+
 // Third-party Libraries
-import { Flex, Grid } from "@chakra-ui/react";
+import { Flex, Grid, Text } from "@chakra-ui/react";
+
+// Components
 import VideoItem from "../components/Card/Video";
 
+// API
+import apiV1 from "../api/apiInstance";
+
+// Chakra Responsive
+const gridColResponsive = {
+  base: "repeat(2, 1fr)",
+  sm: "repeat(3, 1fr)",
+  md: "repeat(4, 1fr)",
+  lg: "repeat(5, 1fr)",
+  xl: "repeat(6, 1fr)"
+}
+
 const Index = () => {
-  const gridColResponsive = {
-    base: "repeat(2, 1fr)",
-    sm: "repeat(3, 1fr)",
-    md: "repeat(4, 1fr)",
-    lg: "repeat(5, 1fr)",
-    xl: "repeat(6, 1fr)"
-  }
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getVideos = async () => {
+      try {
+        const res = await apiV1.get('/videos')
+        
+        setVideos(res.data?.data)
+        setLoading(false)
+      } catch (error) {
+        console.log(error)
+        setLoading(false)
+      }
+    }
+
+    getVideos()
+  })
 
   return (
     <Flex minH='100vh' direction="column">
       <Grid templateColumns={gridColResponsive} gap={4}>
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
+        {
+          loading
+            ? <Text>Loading</Text>
+            : videos?.map((video) => (
+                <VideoItem
+                  key={video._id}
+                  data={video}
+                />
+              ))
+        }
       </Grid>
     </Flex>
   )
