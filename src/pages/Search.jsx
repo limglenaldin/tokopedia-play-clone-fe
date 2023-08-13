8015f54664bc10f77909687c7fb118c15bdc912b
Glenaldin Halim <glenaldinhalim16@gmail.com>
2023-08-14 03:44:21 +0700

    modify search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,13 +1,107 @@
+// Built-in Libraries
+import { useState } from "react";
+
 // Third-party Libraries
-import { Flex, Heading, Text } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Box, Flex, FormControl, FormErrorMessage, Grid, Icon, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useForm } from 'react-hook-form'
+
+// Icons
+import { FiSearch } from "react-icons/fi";
+
+// Component
+import Header from "../components/Header";
+import VideoItem from "../components/Card/Video";
+import VideoSkeleton from "../components/Loading/Skeleton/VideoSkeleton";
+
+// Hooks
+import useUsername from "../hooks/useUsername";
+import apiV1 from "../api/apiInstance";
+
+// Chakra Responsive
+const gridColResponsive = {
+  base: "repeat(2, 1fr)",
+  sm: "repeat(3, 1fr)",
+  md: "repeat(4, 1fr)",
+  lg: "repeat(5, 1fr)",
+  xl: "repeat(6, 1fr)"
+}
 
 const Search = () => {
+  const { username } = useUsername();
+
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting }
+  } = useForm({
+    defaultValues: {
+      keyword: ''
+    }
+  })
+
+  const onSubmitSearch = async (data) => {
+    console.log(data)
+    try {
+      setLoading(true)
+      const response = await apiV1.get(`/videos`, { params: { search: data.keyword } })
+      setVideos(response.data?.data)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
   return (
-    <Flex bg="red.500" direction="column" minH='100vh'>
-      <Heading as='h1' size='lg'>Hello from Search</Heading>
-      <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio natus, id dicta consequuntur iure ratione magni? Deserunt, nam quae.</Text>
-      <Link to="/">Go to Home</Link>
+    <Flex minH='100vh' direction="column" pb="4" gap="8">
+      <Header
+        username={username}
+      />
+      <Box>
+        <form onSubmit={handleSubmit(onSubmitSearch)}>
+          <FormControl isInvalid={errors.keyword}>
+            <InputGroup>
+              <InputLeftElement>
+                <Icon as={FiSearch} />
+              </InputLeftElement>
+              <Input
+                type="text"
+                name="keyword"
+                id="keyword"
+                placeholder="Put your keyword"
+                {...register('keyword')}
+                rounded="full"
+                _disabled={isSubmitting}
+              />
+            </InputGroup>
+            <FormErrorMessage>
+              {errors.keyword && errors.keyword.message}
+            </FormErrorMessage>
+          </FormControl>
+        </form>
+      </Box>
+      <Grid templateColumns={gridColResponsive} gap={4}>
+        {
+          loading
+            ? <>
+              <VideoSkeleton />
+              <VideoSkeleton />
+              <VideoSkeleton />
+              <VideoSkeleton />
+              <VideoSkeleton />
+              <VideoSkeleton />
+            </>
+            : videos?.map((video) => (
+                <VideoItem
+                  key={video._id}
+                  data={video}
+                />
+              ))
+        }
+      </Grid>
     </Flex>
   )
 }
