e8b58ee16f70f353423f714b57ded18cc6a35201
Glenaldin Halim <glenaldinhalim16@gmail.com>
2023-08-14 20:35:48 +0700

    modify vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
   }
 })
