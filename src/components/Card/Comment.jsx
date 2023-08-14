9305495ca7addda7d5d544047c01b06420cba8e2
Glenaldin Halim <glenaldinhalim16@gmail.com>
2023-08-14 18:42:35 +0700

    implement useMoment on comment item card component

--- src/components/Card/Comment.jsx
+++ src/components/Card/Comment.jsx
@@ -1,11 +1,17 @@
 // Third-party Libraries
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Flex, Text } from "@chakra-ui/react"
 import PropTypes from 'prop-types';
+import useMoment from "../../hooks/useMoment";
 
 const CommentItem = ({data}) => {
+  const time = useMoment(data.createdAt)
+
   return (
     <Box py="2">
-      <Text as="b" fontSize="md">{data.username}</Text>
+      <Flex direction="row" justify="space-between">
+        <Text as="b" fontSize="md">{data.username}</Text>
+        <Text fontSize="sm">{time}</Text>
+      </Flex>
       <Text>{data.comment}</Text>
     </Box>
   )
