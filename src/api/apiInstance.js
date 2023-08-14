b01f63a15415e9874799150eefb60cd7fc838f0b
Glenaldin Halim <glenaldinhalim16@gmail.com>
2023-08-14 20:04:23 +0700

    add base url option

--- src/api/apiInstance.js
+++ src/api/apiInstance.js
@@ -1,7 +1,7 @@
 // Third-party Libraries
 import axios from "axios";
 
-const BASE_API_URL = import.meta.env.VITE_BASE_API_URL
+const BASE_API_URL = import.meta.env.VITE_BASE_API_URL ? import.meta.env.VITE_BASE_API_URL : 'http://127.0.0.1:5000'
 
 const apiV1 = axios.create({
     baseURL: `${BASE_API_URL}/api/v1`,
--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -2,7 +2,7 @@
 import { io } from 'socket.io-client';
 
 const useSocket = () => {
-  const BASE_API_URL = import.meta.env.VITE_BASE_API_URL
+  const BASE_API_URL = import.meta.env.VITE_BASE_API_URL ? import.meta.env.VITE_BASE_API_URL : 'http://127.0.0.1:5000'
 
   const socket = io(BASE_API_URL, {
     autoConnect: false
