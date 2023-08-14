b9845026902eff9927d712d763dcea738237473d
Glenaldin Halim <glenaldinhalim16@gmail.com>
2023-08-14 08:10:14 +0700

    add custom hooks useSocket

---
+++ src/hooks/useSocket.js
@@ -0,0 +1,16 @@
+// Third-party Libraries
+import { io } from 'socket.io-client';
+
+const useSocket = () => {
+  const BASE_API_URL = import.meta.env.VITE_BASE_API_URL
+
+  const socket = io(BASE_API_URL, {
+    autoConnect: false
+  })
+
+  socket.connect()
+
+  return { socket }
+}
+
+export default useSocket
